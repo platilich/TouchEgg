6c6e86ede25d67de6c57879524cb85fc275f0247
DarkSpace <platilich@icloud.com>
2026-05-14 12:52:26 +0200

    Fix:
    - #about { cursor: ponter }
    - js: fix btn change theme

--- script.js
+++ script.js
@@ -18,10 +18,11 @@ if (savedTheme) {
 
 
 button.addEventListener("click", function(){
-  let isDark = document.body.classList.toggle("dark")
+  let isDark = document.body.classList.toggle("dark") // если класс .dark не применен применяем, если он есть удаляем
 
-  button.innerHTML = 'Light Mode'
 
-  localStorage.setItem("theme", isDark ? "dark" : "light")
+  button.innerHTML = isDark ? "Light Mode" : "Dark Mode" // если isDark равен True, то тогда кнопка с надписью Light Mode, а если isDark == false, то тогда пишем Dark Mode
+
+  localStorage.setItem("theme", isDark ? "dark" : "light") // если isDark сохраняем тему dark, если у false то пишем light
 
 });
